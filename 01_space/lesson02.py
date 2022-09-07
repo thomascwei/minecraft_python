a489bbb9854a7029e4c72fe71e70a06a8ee5ecaa
thomascwei <thomas.c.wei@gmail.com>
2022-09-07 08:39:38 +0800

    .

--- 01_space/lesson02.py
+++ 01_space/lesson02.py
@@ -12,12 +12,29 @@ mc = Minecraft.create(serverAddress, pythonApiPort, playerName)
 
 
 def chicken_rain():
+    """
+    show time
+    """
     x, y, z = mc.player.getPos()
     for i in range(100):
         mc.spawnEntity(x + random.random() * 10, y + 20, z + random.random() * 10, entity.CHICKEN)
         time.sleep(0.2)
 
 
-if __name__ == '__main__':
-    chicken_rain()
+def demo01():
+    """
+    正上方10格位置生出一隻雞
+    """
+    x, y, z = mc.player.getPos()
+    mc.spawnEntity(x, y + 10, z, entity.CHICKEN)
 
+
+def practice01():
+    """
+    製作一個五層高的柱子,在註解下方寫下你的程式碼
+    """
+
+
+if __name__ == '__main__':
+    demo01()
+    chicken_rain()
